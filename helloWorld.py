6f40237b6fb9c74fd207639ffcb500ac1f492a55
NoniMncina <nonhlanhlamncina@gmail.com>
2024-05-04 11:27:37 +0200

    Include a welcome message before requesting user input.

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+# Welcome message
+print("Welcome to the input program!")
+
+# Accept input from the user
+user_input = input("Please enter some data: ")
+
+# Print out the inputted data
+print("You entered:", user_input)
